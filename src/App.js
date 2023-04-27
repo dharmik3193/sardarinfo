d37cf7fbcab59be359f91b1f2541b89c626d8e19
dharmik3193 <dnbambharoliya555@gmail.com>
2023-04-27 22:42:54 +0530

    upfated timer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [email, setEmail] = useState('');
+  const [alert, setAlert] = useState('')
+
+  let x = setInterval(function () {
+
+    let now = new Date()
+    let year = 2024
+    let month = now.getMonth()
+    if (month === 0) {
+      year = now.getFullYear() + 1;
+    }
+
+
+    let comingDate = new Date(`${month + 1} 31, ${year} 13:12:00`)
+    // let now = new Date()
+    let selisih = comingDate.getTime() - now.getTime()
+
+    setDays(Math.floor(selisih / (1000 * 60 * 60 * 24)))
+    setHours(Math.floor(selisih % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))
+    setMinutes(Math.floor(selisih % (1000 * 60 * 60) / (1000 * 60)))
+    setSeconds(Math.floor(selisih % (1000 * 60) / 1000))
+
+    if (selisih < 0) {
+      clearInterval(x)
+      setDays(0)
+      setHours(0)
+      setMinutes(0)
+      setSeconds(0)
+    }
+
+
+  }, 1000)
+
+  const formhandler = (e) => {
+    setEmail(e.target.value)
+    if (e.target.value === '') {
+      setAlert("* This is required field")
+    } else if (!/^\S+@\S+\.\S+$/.test(e.target.value)) {
+      setAlert("Email Must Contain @ and .")
+    } else {
+      setAlert("")
+    }
+
+  }
+
+  const reset = () => {
+    setEmail("")
+  }
+
   var loadScript = function (src) {
     var tag = document.createElement('script');
     tag.async = false;
@@ -13,7 +67,7 @@ function App() {
   useEffect(() => {
     loadScript('js/jquery.min.js')
     loadScript('js/bootstrap.min.js')
-    loadScript('js/jquery.countdown.min.js')
+    // loadScript('js/jquery.countdown.min.js')
     loadScript('js/jquery.animatedheadline.min.js')
     loadScript('js/pace.min.js')
     loadScript('js/main.js')
@@ -25,17 +79,17 @@ function App() {
         {/* Background Element Area Start Here */}
         <ul className="fxt-bg-element">
           <li className="top-right fxt-transformX-100 fxt-transition-delay-2">
-            <img src="img/figure/shape2.png" alt="Shap" />
+            <img src="img/figure/shape2.png" alt="Shape" />
           </li>
           <li className="bottom-left fxt-transformX--100 fxt-transition-delay-2">
-            <img src="img/figure/shape1.png" alt="Shap" />
+            <img src="img/figure/shape1.png" alt="Shape" />
           </li>
         </ul>
         {/* Background Element Area End Here */}
         {/* Logo Area Start Here */}
         <div className="fxt-logo fxt-logo-layout5 fxt-absolute-box fxt-box-top-left">
           <div className="fxt-transformX--100 fxt-transition-delay-12">
-            <img src="img/dark-logo.png" alt="logo" />
+            <img src="img/dark-logo.png" alt="Sardar Infotech" title='Sardar Infotech'/>
           </div>
         </div>
         {/* Logo Area End Here */}
@@ -46,11 +100,11 @@ function App() {
             data-line-options='{"animationType": "rotate-2", "animationDelay": 3000 }'
           >
             <div className="ah-headline item-title-thin fxt-transformY-100 fxt-transition-delay-12">
-              <span className="fixed-text">We Are</span>
+              <span className="fixed-text">We Are </span>
               <span className="animate-text ah-words-wrapper">
-                <b className="is-visible">Building...</b>
-                <b>Construction...</b>
-                <b>Kitchen Cabinate...</b>
+                <b className="is-visible"> Building...</b>
+                <b> Construction...</b>
+                <b> Kitchen Cabinate...</b>
               </span>
             </div>
           </div>
@@ -58,7 +112,32 @@ function App() {
             Something Awesome
           </h1>
           <div className="countdown-layout2 fxt-transformY-100 fxt-transition-delay-16">
-            <div className="countdown" />
+            <div class="countdown">
+              <div class="countdown-section">
+                <div>
+                  <div class="countdown-number">{days}</div>{console.log(days)}
+                  <div class="countdown-unit">Days</div>
+                </div>
+              </div>
+              <div class="countdown-section">
+                <div>
+                  <div class="countdown-number">{hours}</div>
+                  <div class="countdown-unit">Hours</div>
+                </div>
+              </div>
+              <div class="countdown-section">
+                <div>
+                  <div class="countdown-number">{minutes}</div>
+                  <div class="countdown-unit">Minutes</div>
+                </div>
+              </div>
+              <div class="countdown-section">
+                <div>
+                  <div class="countdown-number">{seconds}</div>
+                  <div class="countdown-unit">Second</div>
+                </div>
+              </div>
+            </div>
           </div>
           <p className="fxt-transformY-100 fxt-transition-delay-18">
             Perfect and awesome template to present your future product or service.
